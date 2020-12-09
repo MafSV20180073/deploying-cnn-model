bbbbb2725fa1fbda6a0061bc9dc42dd60847039b
MafSV20180073 <42385657+MafSV20180073@users.noreply.github.com>
2020-12-09 14:38:21 +0000

    Fix directory error that was throwing internal server error

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-import numpy as np 
+import numpy as np
 import os
 
-from flask import Flask, render_template, session, redirect, url_for, session, request
+from flask import Flask, render_template, request
 from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 from werkzeug.utils import secure_filename
-from wtforms import TextField, SubmitField
+from wtforms import SubmitField
 
 
 base_dir = os.path.abspath(os.path.dirname(__file__))
@@ -22,7 +22,7 @@ configure_uploads(app, photos)
 patch_request_class(app)  # set maximum file size, default is 16MB
 
 # Load the model:
-cnn_model = load_model('<write-here-the-name-of-the-file>.h5')
+cnn_model = load_model('cats_and_dogs_model9.h5')
 CLASS_INDICES = {0: 'cat', 1: 'dog'}
 
 
@@ -32,29 +32,23 @@ class UploadForm(FlaskForm):
     submit = SubmitField('Get Prediction')
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def index():
-    form = UploadForm()
-    # If the form is valid on submission:
-    if form.validate_on_submit():
-        # Saving file to folder
-        file = request.files['photo']
-        filename = secure_filename(file.filename)
-        file.save(os.path.join('uploads', filename))
-        return redirect(url_for("prediction", filename=filename))
-    else: 
-        return render_template('home.html', form=UploadForm())
+    return render_template('home.html', form=UploadForm())
 
 
-@app.route('/prediction/<filename>')
-def prediction(filename):
-    results = return_prediction(model=cnn_model, filename=filename)
+@app.route('/prediction/', methods=['POST'])
+def prediction():
+    # Saving file to folder
+    file = request.files['photo']
+    filename = secure_filename(file.filename)
+    file.save(os.path.join('uploads', filename))
+
+    results = return_prediction(filename=filename)
     return render_template('prediction.html', results=results)
 
 
-def return_prediction(model, filename):
-    # Note: in some computers such as Macs, the line below may throw an error. If that's your case, add 'uploads/' before the filename.
-    # e.g.: _image_process('uploads/' + filename)
+def return_prediction(filename):
     input_image_matrix = _image_process(filename)
     score = cnn_model.predict(input_image_matrix)
     class_index = cnn_model.predict_classes(input_image_matrix, batch_size=1)
@@ -63,7 +57,7 @@ def return_prediction(model, filename):
 
 
 def _image_process(filename):
-    img = image.load_img(filename, target_size=(64, 64))
+    img = image.load_img('uploads/' + filename, target_size=(64, 64))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     input_matrix = np.vstack([x])
